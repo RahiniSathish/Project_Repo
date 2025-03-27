0f364087d294e80370383b2870b533f3dffd4126
Rahini_Sathish <141403412+RahiniSathish@users.noreply.github.com>
2025-03-27 18:38:38 +0530

    Update chat_engine

--- chat_engine.py
+++ chat_engine.py
@@ -2,7 +2,6 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_openai import AzureChatOpenAI
 from config import AZURE_ENDPOINT, API_KEY, API_VERSION, DEPLOYMENT_ID
-
 llm = AzureChatOpenAI(
     azure_endpoint=AZURE_ENDPOINT,
     api_key=API_KEY,
@@ -11,15 +10,11 @@ llm = AzureChatOpenAI(
     openai_api_type="azure",
     temperature=0,
 )
-
-
 prompt = PromptTemplate(
     input_variables=["question"],
     template="Answer the following question as clearly as possible:\n\nQuestion: {question}\nAnswer:"
 )
-
 chain = LLMChain(prompt=prompt, llm=llm)
-
 def get_answer(question: str) -> str:
     try:
         return chain.run(question)
